43d3ad7e6d739d841f19e4f9fcdae031d9cb3fb2
SirDimwi <170129303+SirDimwi@users.noreply.github.com>
2026-06-18 07:29:05 -0400

    Set production URL to Railway domain

--- extension/config.js
+++ extension/config.js
@@ -1,2 +1,2 @@
 // Change this to your deployed Railway/Fly.io URL before publishing
-const DASHBOARD_URL = 'http://localhost:3000';
+const DASHBOARD_URL = 'https://nlbrowser-production.up.railway.app';
